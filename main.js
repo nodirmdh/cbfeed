c0fc66c187d4635f1a40ed33f4087da1cbb36840
Nodir Makhmudov <makhmudovnodir111@gmail.com>
2026-04-05 22:38:11 +0500

    финал

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const toTopButtons = document.querySelectorAll(".to-top");
+
+  toTopButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  });
+});
